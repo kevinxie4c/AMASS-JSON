f5eddd50add3db09768cb3913718ea73e1cf30c2
Kaixiang Xie <kaixiang.xie@mail.mcgill.ca>
2022-08-31 20:41:33 -0400

    add shape
    fix bugs

--- make-json.pl
+++ make-json.pl
@@ -61,6 +61,7 @@ lHand
 rHand
 );
 my $num_joints = @joint_names;
+my $width = 0.05;
 
 sub read_list {
     my $fname = shift @_;
@@ -82,15 +83,28 @@ my $moi = read_list("$dir/inertia_tensor.txt");
 
 my @kintree = @{read_list("$dir/kintree.txt")->[0]};
 
+sub list_sub {
+    my ($a, $b) = @_;
+    die "lists don't have the same length" if @$a != @$b;
+    map { $a->[$_] - $b->[$_] } (0 .. $#$a);
+}
+
 my $i = 0;
 my @joints;
 for my $name (@joint_names) {
+    my $parent = $kintree[$i];
+    my @pos;
+    if ($parent < 10000) {
+	@pos = list_sub($pos->[$i], $pos->[$parent]);	# from global to local frame
+    } else {
+	@pos = @{$pos->[$i]};
+    }
     my $joint = {
 	name	=> $name,
-	pos	=> join(', ', @{$pos->[$i]}),
-	mass	=> join(', ', @{$mass->[$i]}),
-	COM	=> join(', ', @{$com->[$i]}),
-	MOI	=> join(', ', @{$moi->[$i]}),
+	pos	=> [map 0+$_, @pos],	# position in parent joint's local frame
+	mass	=> 0+$mass->[$i][0],
+	COM	=> [map 0+$_, @{$com->[$i]}],
+	MOI	=> [map 0+$_, @{$moi->[$i]}],
 	type	=> "ball",
     };
     push @joints, $joint;
@@ -102,8 +116,25 @@ $joints[0]{type} = "free";
 $i = 0;
 for my $joint (@joints) {
     my $parent = $kintree[$i];
+    my @a_pos = @{$joint->{pos}};
     if ($parent < 10000) {
 	push @{$joints[$parent]{children}}, $joint;
+	my @b_pos = @{$joints[$parent]{pos}};
+	# find the long axis
+	my $j = 0;
+	my @box_pos;
+	for (my $k = 0; $k < 3; ++$k) {
+	    $j = $k if abs($a_pos[$k]) > abs($a_pos[$j]);
+	    $box_pos[$k] = $a_pos[$k] / 2;
+	}
+	my @size = ($width) x 3;
+	$size[$j] = abs($a_pos[$j]);
+	my $shape = {
+	    type => 'box',
+	    size => \@size,
+	    pos  => \@box_pos,
+	};
+	push @{$joints[$parent]{shape}}, $shape;
     }
     ++$i;
 }
